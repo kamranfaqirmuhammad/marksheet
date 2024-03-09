de6d377d3eaea5a80c8c67022bd49122abd03dfd
kamranfaqirmuhammad <159239552+kamranfaqirmuhammad@users.noreply.github.com>
2024-03-10 01:15:37 +0500

    Add files via upload

---
+++ operator.ts
@@ -0,0 +1,89 @@
+// let stdName = "kamran"
+// let isSlot = "tuesday"
+// let isAllowed = stdName == 'kamran'
+
+// console.log(isAllowed);
+
+// let sN = "Ali"
+// let lS = "Tuesday"
+// let iA = sN === 'Ali' && lS === 'Tuesday'
+
+// console.log(iA)
+
+// let fValue =true
+// let lValue = false
+// let iA = fValue || lValue
+
+// console.log(iA)
+
+// console.log(!true)
+
+// console.log(!(5<4))
+
+// if (condition){
+//     Any code
+// }
+
+// let isPassed = true
+// if (isPassed){
+//     console.log("Bike mil gae")
+// }
+
+// else{
+//     console.log("Saddar jana k kitnay hay")
+// }
+
+// if (5<4){
+//     console.log("if wala block")
+// }
+// else{
+//     console.log("else wala block")
+// }
+
+// let per = 45
+
+// if (per === 90){
+//     console.log("A+")
+// }
+// else if (per >= 80 || (per<=89)){
+//     console.log("A1")
+// }
+// else if (per >= 70 || (per<=79)){
+//     console.log("A")
+// }    
+// else if (per >= 60 || (per<=69)){
+//     console.log("B")
+// }
+// else if (per >= 50 || (per<=59)){
+//     console.log("C")
+// }
+// else if  (per >= 40 || (per<=49)){
+//     console.log("D")
+// }
+// else {
+//     console.log("Fail")
+// }
+
+let per = 35
+
+if (per = 90){
+    console.log("A+")
+}
+else if (per >= 80 ){
+    console.log("A1")
+}
+else if (per >= 70 ){
+    console.log("A")
+}    
+else if (per >= 60 ){
+    console.log("B")
+}
+else if (per >= 50 ){
+    console.log("C")
+}
+else if  (per >= 40 ){
+    console.log("D")
+}
+else {
+    console.log("Fail")
+}
